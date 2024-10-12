b10681115cf328981cc96103a50373d434c733f9
wizozero <wizodev@gmail.com>
2024-10-12 19:53:47 +0200

    Implement BST construction and node insertion

--- tree.js
+++ tree.js
@@ -15,7 +15,7 @@ class Tree {
 			if (start > end) return null
 
 			const mid = Math.floor((start + end) / 2)
-			const root = new Node(removedDuplicatesArr)
+			const root = new Node(removedDuplicatesArr[mid])
 
 			root.left = buildSubTree(start, mid - 1)
 			root.right = buildSubTree(start + 1, end)
@@ -24,5 +24,30 @@ class Tree {
 		}
 
 		return buildSubTree(0, removedDuplicatesArr.length - 1)
+	}
+
+	insert(value) {
+		if (this.root === null) {
+			this.root = new Node(value)
+			return
+		}
+
+		const insertNode = (node, value) => {
+			if (value < node.data) {
+				if (node.left === null) {
+					node.left = new Node(value)
+				} else {
+					insertNode(node.left, value)
+				}
+			} else if (value > node.data) {
+				if (node.right === null) {
+					node.right = new Node(value)
+				} else {
+					insertNode(node.right, value)
+				}
+			}
+		}
+
+		insertNode(this.root, value)
 	}
 }
