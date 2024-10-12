27ee170d1279c77ee2168601ce2b10ebdde86389
wizozero <wizodev@gmail.com>
2024-10-12 19:21:34 +0200

    Implement buildTree method for balanced BST construction

--- node.js
+++ node.js
@@ -1,4 +1,4 @@
-class Node {
+export default class Node {
 	constructor(data) {
 		this.data = data
 		this.left = null
--- tree.js
+++ tree.js
@@ -1,9 +1,28 @@
+const Node = require('./node')
+
 class Tree {
 	constructor(arr) {
 		this.root = this.buildTree(arr)
 	}
 
 	buildTree(arr) {
-		return null
+		const sortedArr = arr.sort((a, b) => a - b)
+		const removedDuplicatesArr = sortedArr.filter(
+			(item, index) => sortedArr.indexOf(item) === index
+		)
+
+		const buildSubTree = (start, end) => {
+			if (start > end) return null
+
+			const mid = Math.floor((start + end) / 2)
+			const root = new Node(removedDuplicatesArr)
+
+			root.left = buildSubTree(start, mid - 1)
+			root.right = buildSubTree(start + 1, end)
+
+			return root
+		}
+
+		return buildSubTree(0, removedDuplicatesArr.length - 1)
 	}
 }
